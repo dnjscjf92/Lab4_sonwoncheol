0cdd11f0dcb88f91423fca45466295f08a931513
jungyong12 <comrry@ajou.ac.kr>
2016-10-04 10:47:19 +0900

    dfsa

---
+++ Lab4_201221764/src/Calculator.java
@@ -0,0 +1,36 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public double add(double x, double y){
+		
+		return 0;
+	}
+	
+	public double sub(double x,double y){
+		
+		return 0;
+	}
+	
+	public double div(double x, double y){
+		
+		return 0;
+	}
+	
+	public double mul(double x, double y){
+		
+		return 0;
+	}
+	
+	public double mod(double x,double y){
+		
+		return 0;
+	}
+	
+	
+}
