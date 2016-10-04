5f6e41e151eda5498c3d5602efbfce49a3e9718d
jungyong12 <comrry@ajou.ac.kr>
2016-10-04 11:20:12 +0900

    asfasf

--- Lap4/src/Calculator.java
+++ Lap4/src/Calculator.java
@@ -1,7 +1,40 @@
 
 public class Calculator {
 
-	public double add (double x, double y){
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public double add(double x, double y){
+		
+		return x+y;
+	}
+	
+	public double sub(double x,double y){
+		
 		return 0;
+	}
+	
+	public double div(double x, double y){
+		
+		return 0;
+	}
+	
+	public double mul(double x, double y){
+		
+		return x*y;
+	}
+	
+	public double mod(double x,double y){
+		
+		return 0;
+	}
+	
+	public String toString(){
+	String a = "LeeHakSeung,201221014,ChoiJungonyg,201221764,Sonwoncheol,201220598,https://github.com/hakseunglee/lap4_calculator.git";
+	
+	return a;
 	}
 }
