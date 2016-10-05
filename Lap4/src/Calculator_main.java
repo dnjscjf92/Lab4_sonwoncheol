8d72db1c9d07b4904dde9ed6caf9afcfec7f82b4
sonwoncheol <1chuu@naver.com>
2016-10-05 23:57:08 +0900

    h

--- Lap4/src/Calculator_main.java
+++ Lap4/src/Calculator_main.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
-public class Calculator_test {
+public class Calculator_main {
 
 	
 	public static void main(String[] arg){
 		Scanner sc = new Scanner(System.in);
 		Calculator c = new Calculator();
-		System.out.println("input : ");
+		System.out.println("input X : ");
 		double x = sc.nextDouble();
+		System.out.println("input Y : ");
 		double y = sc.nextDouble();
 		System.out.println("result (add) : "+ c.add(x, y));
 		System.out.println("result (sub) : "+ c.sub(x, y));
